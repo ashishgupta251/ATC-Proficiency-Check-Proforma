f7329f084ec4ab7e6fbfd8227a628ed16357085f
Ashish Gupta <agashish2517@gmail.com>
2026-06-19 14:18:25 +0530

    Added Completed Files

---
+++ js/auth.js
@@ -0,0 +1,22 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const id = document.getElementById("userId").value;
+
+  const password = document.getElementById("password").value;
+
+  const user = USERS.find((u) => u.id === id && u.password === password);
+
+  if (!user) {
+    alert("Invalid ID or Password");
+    return;
+  }
+
+  sessionStorage.setItem("user", JSON.stringify(user));
+
+  if (user.role === "admin") {
+    location.href = "admin-dashboard.html";
+  } else {
+    location.href = "dashboard.html";
+  }
+});
